c09ddc7f4ee1eeadd2a1e39cd0b30ff13aa2813a
Oleg Maksimov <omaksimov@inno.tech>
2026-01-16 20:39:06 +0300

    Программа арифметических действий с двумя числами

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-                //+ Math.random());
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Программа вычисления суммы, разности, произведения и частного двух чисел.");
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        double quotient = (double) number1  /number2;
+        int sum = number1 + number2;
+        int diff = number1 - number2;
+        int product = number1 * number2;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + diff);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
     }
 }
