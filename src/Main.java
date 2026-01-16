65b92fa4608c40a20aa255c23d1d1851f3d1d4d5
Oleg Maksimov <omaksimov@inno.tech>
2026-01-16 19:38:44 +0300

    "Длина текста: "

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите текст и нажмите <Enter>: ");
+                //+ Math.random());
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
+    }
+}
