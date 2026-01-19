1418bd8d2fcadecb0439c8e7def15712cd8db54e
Oleg Maksimov <omaksimov@inno.tech>
2026-01-19 22:32:45 +0300

    "Результат сложения двух последних знаков числа: " + sumLastNums(number1)

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Программа вычисления суммы, разности, произведения и частного двух чисел.");
+        System.out.println("Программа вычисления.");
         System.out.println("Введите первое число:");
         int number1 = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int number2 = new Scanner(System.in).nextInt();
-        double quotient = (double) number1  /number2;
-        int sum = number1 + number2;
-        int diff = number1 - number2;
-        int product = number1 * number2;
-        System.out.println("Сумма: " + sum);
-        System.out.println("Разность: " + diff);
-        System.out.println("Произведение: " + product);
-        System.out.println("Частное: " + quotient);
+        //System.out.println("Введите второе число:");
+        //int number2 = new Scanner(System.in).nextInt();
 
-        System.out.println("Дробная часть: " + desimal(quotient));
+        System.out.println("Результат сложения двух последних знаков числа: " + sumLastNums(number1));
     }
 
-    public static double desimal(double x){
-    int  y = (int) x;
-    double z =  (x - y) * 1000;
-        System.out.println("debug z: " + z);
-    int f = (int) z;
-    double df = (double) f / 1000;
-    return df;
+    public static int sumLastNums(int input) {
+    int LastOne = input % 10;
+    int LastTwo = (input / 10) % 10;
+    System.out.println("LastOne: " + LastOne + " LastTwo: " + LastTwo);
+    return LastOne + LastTwo;
 
-    //DecimalFormat df = new DecimalFormat("#.###"); // Шаблон #.### для 3 знаков
-    //return df.format(z);
 }
-
-
 }
