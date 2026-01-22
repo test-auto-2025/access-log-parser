d6c79d4988f08390471fa9ae11f85d25a2b18b3d
Oleg Maksimov <omaksimov@inno.tech>
2026-01-22 21:18:52 +0300

    Результат проверки числа  на двузначность

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Программа вычисления ASCII кода");
-        System.out.println("Введите любую цифру от 0 до 9");
-        int InputNumber = new Scanner(System.in).nextInt(); // Считывает как слово
-        System.out.println("Результат проверки числа " + InputNumber + " на положительность: is " + isPositive(InputNumber));
+        System.out.println("Программа проверки вводимых данных");
+        System.out.println("Введите любое положительное число");
+        int InputNumber = new Scanner(System.in).nextInt();
+        if (InputNumber > 0) {
+            System.out.println("Результат проверки числа " + InputNumber + " на двузначность: is " + is2Digits(InputNumber));
+        } else {
+            System.out.println("Вы ввели отрицательное число!");
+        }
+
     }
 
-    public static boolean isPositive(int x) {
-
-        if (x > 0) {
+    public static boolean is2Digits(int x) {
+        if ((x >= 10 && x <= 99) || (x <= -10 && x >= -99)) {
             return true;
         } else {
             return false;
-        }  // Возвращает результат проверки числа на положительность. Если число положительное, то возвращается true, иначе - false.
+        }
     }
 
 
