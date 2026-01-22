692de506d97acccda155595937efc613b9fa57b3
Oleg Maksimov <omaksimov@inno.tech>
2026-01-22 20:55:59 +0300

    возвращать true если число положительное

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,19 @@ public class Main {
 
         System.out.println("Программа вычисления ASCII кода");
         System.out.println("Введите любую цифру от 0 до 9");
-        String singleCharStr = new Scanner(System.in).next(); // Считывает как слово
-        char ch1 = 'A';
-        if (singleCharStr.length() > 0) {
-            ch1 = singleCharStr.charAt(0); // Получаем первый символ
-            System.out.println("Вы ввели символ: " + ch1);
-        }
-        System.out.println("Результат преобразования символа " + ch1 + " в число: " + charToNum(ch1));
+        int InputNumber = new Scanner(System.in).nextInt(); // Считывает как слово
+        System.out.println("Результат проверки числа " + InputNumber + " на положительность: is " + isPositive(InputNumber));
     }
 
-    public static int charToNum(char x)
-    {
-        // x - символ, который нужно преобразовать в число
-        int AdciiCode = (int) x;
-        return AdciiCode - 48;
+    public static boolean isPositive(int x) {
+
+        if (x > 0) {
+            return true;
+        } else {
+            return false;
+        }  // Возвращает результат проверки числа на положительность. Если число положительное, то возвращается true, иначе - false.
     }
+
 
 }
+
