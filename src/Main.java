b4055696aaa0e7d6dd2cfcc0fbb246a6150356e8
Oleg Maksimov <omaksimov@inno.tech>
2026-01-28 19:31:45 +0300

    сумма последних цифр

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Введите первое числ");
         int a = new Scanner(System.in).nextInt();
-        int b = 0;
         for (int i = 1; i <= 4; i++) {
             System.out.println("Введите число");
-            b = new Scanner(System.in).nextInt();
-            a = lastNumSum(a,b);
+            a = lastNumSum(a,new Scanner(System.in).nextInt());
             System.out.println("Подитог: " + a);
         }
         System.out.println("Результат суммы последних цифр: " + a);
