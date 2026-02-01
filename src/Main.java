9cab615618100e59c77ba9ce8bb1b1fce40bf951
Oleg Maksimov <omaksimov@inno.tech>
2026-02-01 22:11:18 +0300

    максимум чисел

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-       int value = -1000;
-
-        System.out.println("Модуль числа " + value + " = " + abs(value));
+       int a = 1000;
+       int b = 5;
+       int c = -3;
+       int d = 0;
+        System.out.println("максимум из чисел " + a + " и " + b + " = " + max(a,b));
+        System.out.println("максимум из чисел  " + a + " и " + c + " = " + max(a,c));
+        System.out.println("максимум из чисел  " + a + " и " + d + " = " + max(a,d));
     }
 
-    public static int abs(int x){
-        int res =  x>0 ? x : -x;
-        return res;
-    };
+    public static int max(int x, int y){
+        return x > y ? x : y;
+      };
+
 }
 
