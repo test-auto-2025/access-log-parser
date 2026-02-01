686598ffc7403516091f2657a8f42499b88c1f4b
Oleg Maksimov <omaksimov@inno.tech>
2026-02-01 21:59:22 +0300

    Модуль числа

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Программа проверки регистра буквы");
-        System.out.println("Введите любую букву");
-        String singleCharStr = new Scanner(System.in).next(); // Считывает как слово
-        char ch1 = 'A';
-        if (singleCharStr.length() > 0) {
-            ch1 = singleCharStr.charAt(0); // Получаем первый символ
-            System.out.println("Вы ввели символ: " + ch1);
-        };
-        System.out.println("Буква в верхнем реистре: is " + isUpperCase(ch1));
+       int value = -1000;
+
+        System.out.println("Модуль числа " + value + " = " + abs(value));
     }
 
-    public static boolean isUpperCase(char a) {
-        int asciicode = charToNum(a);
-        System.out.println(asciicode);
-        if ((asciicode > 64 && asciicode < 91) || (asciicode > 1039 && asciicode < 1072))
-            // Проверка для английских букв                Проверка для русских букв
-        {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static int charToNum(char x) {
-        // x - символ, который нужно преобразовать в число
-        return (int) x;
-    }
-    ;
+    public static int abs(int x){
+        int res =  x>0 ? x : -x;
+        return res;
+    };
 }
 
