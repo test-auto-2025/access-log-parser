98367f291b0a5175b662a5f7a882c69eb976f837
Oleg Maksimov <omaksimov@inno.tech>
2026-02-02 18:55:01 +0300

    Делится на цело на 3 или на 5

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 
 public class Main {
     public static void main(String[] args) {
-       int a = 12;
-       int b = 8;
+       int a = 8;
+       int b = 15;
        int c = 9;
-        System.out.println("Число " + a + " Делится на цело на 3 и на 5: is " + is35(a));
-        System.out.println("Число " + b + " Делится на цело на 3 и на 5: is " + is35(b));
-        System.out.println("Число " + c + " Делится на цело на 3 и на 5: is " + is35(c));
+        System.out.println("Число " + a + " Делится на цело на 3 или на 5: is " + is35(a));
+        System.out.println("Число " + b + " Делится на цело на 3 или на 5: is " + is35(b));
+        System.out.println("Число " + c + " Делится на цело на 3 или на 5: is " + is35(c));
 
     }
 
     public static boolean is35(int x){
-        return x % 3 == 0 || x % 5 == 0;
+        return (x % 3 == 0 || x % 5 == 0) && (x % 15 != 0);
+
     }
 }
 
