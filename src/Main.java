bb3033394b148019bdc228b98b3ab6a8d4fe6dbc
Oleg Maksimov <omaksimov@inno.tech>
2026-02-09 20:38:47 +0300

    Циклы 7

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 9;
-        System.out.println(numLen(a));
-    }
-
-    public static int numLen(long x){
-
+        int a = 777777777;
+        int b = 1122;
+        System.out.println("Проверка цифры числа " + a + " на одинаковость: " + equalNum(a));
+        System.out.println("Проверка цифры числа " + b + " на одинаковость: " + equalNum(b));
     }
 
 
@@ -17,6 +14,32 @@ public class Main {
 
 
 
+
+
+
+
+
+
+    public static boolean equalNum(int x){
+        int y = x % 10;
+        while (x>10) {
+            x = x / 10;
+            if (y != x % 10) return false;
+        }
+        return true;
+    }
+
+
+    public static int numLen(long x) //количество знаков в числе x
+    {
+        int y = 1;
+        for (int i = 1; x >= 10; i++) {
+            x = x / 10;
+            y = y + 1;
+        }
+        return y;
+
+    }
 
     public static int pow(int x, int y) {
         int result = 1;
@@ -26,13 +49,11 @@ public class Main {
         return result;
     }
 
-
     public static String chet(int x) {
         for (int i = 0; i < x; i += 2)
             System.out.print(i + " ");
         return "";
     }
-
 
     public static String reverseListNums(int x) {
         for (int i = x; i >= 0; i--)
@@ -46,6 +67,5 @@ public class Main {
         return "";
 
     }
-
 
 }
