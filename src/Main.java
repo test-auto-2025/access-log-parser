7b7a5e7ef69c8390525026733add8760ceadd3f1
Oleg Maksimov <omaksimov@inno.tech>
2026-02-11 20:50:58 +0300

    Курсовой проект. Задание по теме "Циклы"

--- src/Main.java
+++ src/Main.java
@@ -1,116 +1,38 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        guessGame();
-    }
+        int successTries = 0;
+        int losedTries = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static void guessGame() {
-        int randomNum = 3;
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        System.out.println("What number am I thinking (0 to 9)? :");
-        int x = 0;
-        do {
-            x = sc.nextInt();
-            if (x != randomNum) {
-                System.out.println("No, try again");
-            } else {
-                System.out.println("Yes, it`s " + randomNum);
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory) {
+                System.out.println("Это папка, а не файл!");
+                losedTries++;
+                System.out.println("Неудачных попыток: " + losedTries);
+                continue;
             }
-        } while (x != randomNum);
-    }
 
-    public static void rightTriangle(int x){
-        for (int i = 1; i <= x; i++) {
-            for (int j = 1; j <= x - i; j++) {
-                System.out.print(" ");
+            boolean fileExist = file.exists();
+            if (!fileExist) {
+                System.out.println("Неверно указан путь или файл не найден");
+                losedTries++;
+                System.out.println("Неудачных попыток: " + losedTries);
+                continue;
             }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-            System.out.println("");
+
+            System.out.println("Ура! Файл найден!");
+            successTries++;
+            System.out.println("Успешных попыток: " + successTries);
         }
-    }
-
-    public static void leftTriangle(int x){
-
-        for (int i = 1; i <= x; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-    }
-
-    public static void square(int x) {
-
-        for (int i = 1; i <= x; i++) {
-            for (int j = 1; j <= x; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-    }
-
-    public static boolean equalNum(int x) {
-        int y = x % 10;
-        while (x > 10) {
-            x = x / 10;
-            if (y != x % 10) return false;
-        }
-        return true;
-    }
-
-    public static int numLen(long x) //количество знаков в числе x
-    {
-        int y = 1;
-        for (int i = 1; x >= 10; i++) {
-            x = x / 10;
-            y = y + 1;
-        }
-        return y;
 
     }
 
-    public static int pow(int x, int y) {
-        int result = 1;
-        for (int i = 0; i < y; i++) {
-            result *= x;
-        }
-        return result;
-    }
-
-    public static String chet(int x) {
-        for (int i = 0; i < x; i += 2)
-            System.out.print(i + " ");
-        return "";
-    }
-
-    public static String reverseListNums(int x) {
-        for (int i = x; i >= 0; i--)
-            System.out.print(i + " ");
-        return "";
-    }
-
-    public static String listNums(int x) {
-        for (int i = 0; i <= x; i++)
-            System.out.print(i + " ");
-        return "";
-
-    }
 
 }
