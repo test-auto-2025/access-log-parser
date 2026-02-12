5920dcd73f478094ca0f4d65690beaa932dac0a4
Oleg Maksimov <omaksimov@inno.tech>
2026-02-12 21:10:30 +0300

    Практика Java. Массивы #2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8};
-        System.out.println (findFirst(arr1, 6));
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 5, 5, 8};
+        System.out.println (findLast(arr1, 7));
     }
 
-    public static int findFirst(int[] arr, int x) {
-        for (int i = 0; i < arr.length; i++) {
+    public static int findLast(int[] arr, int x) {
+        for (int i = arr.length-1; i > 0; i--) {
             if (x == arr[i]) {
-                System.out.println("элемент с номером" + i );
                 return i;
             }
         }
-        System.out.println("-1"); //элемент не найден
+       //System.out.println("-1"); //элемент не найден
         return -1;
     }
 }
