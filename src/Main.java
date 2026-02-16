81853a64d4884fffa38c0a8d9a444ef06a98aefe
Oleg Maksimov <omaksimov@inno.tech>
2026-02-16 23:35:02 +0300

    Практика Java. Массивы #5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 5, 5, 8};
-        System.out.println (findLast(arr1, 7));
+        int[] arr1 = {1, 2, -3, 4, -5, -6, 7, 5, 5, -8};
+        System.out.println (countPositive(arr1));
+    }
+
+    public static int countPositive(int[] arr)
+    {
+        int countPositive = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                countPositive++;
+            }
+        }
+        return countPositive;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static int maxAbs(int[] arr)
+    {
+        for (int i= 0; i < arr.length; i++) {
+            arr[i] = Math.abs(arr[i]);
+        }
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 
     public static int findLast(int[] arr, int x) {
