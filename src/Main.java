9aa5ee1513d22aea917513343ca0a4fd413f235d
Oleg Maksimov <omaksimov@inno.tech>
2026-02-17 20:06:54 +0300

    Практика Java. Массивы #7

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, -3, 4, -5, -6, 7, 5, 5, -8};
-        int[] arr2 = {-8, 5, 5, 7, -6, 7, 5, 5, -8};
-        reverse(arr1);
-        System.out.println ("Массив 1 " );
-        for (int i=0; i <= arr1.length - 1; i++){
-            System.out.print(arr1[i] + ", "); //выводим массив на экран через запятую
-        }
-        System.out.println ("");
-        System.out.println ("Массив 2 " );
-        reverse(arr2);
-        for (int i=0; i <= arr2.length - 1; i++){
-            System.out.print(arr2[i] + ", "); //выводим массив на экран через запятую
+        int[] arr3 = reverseBack(arr1);
+        System.out.println("Массив 1");
+        for (int i = 0; i <= arr3.length - 1; i++) {
+            System.out.print(arr3[i] + ", "); //выводим массив на экран через запятую
         }
     }
 
-    public static void reverse(int[] arr){
+    public static int[] reverseBack(int[] arr) {
+        int[] result = new int[arr.length];
+        for (int i = 0; i <= arr.length - 1; i++) {
+            result[i] = arr[arr.length - 1 - i];
+        }
+        return result;  //метод меняет порядок элементов массива на обратный
+    }
+
+
+
+
+
+
+
+
+    public static void reverse(int[] arr) {
 
         int left = 0;
         int right = arr.length - 1;
@@ -28,16 +36,7 @@ public class Main {
         }  //метод меняет порядок элементов массива на обратный
     }
 
-
-
-
-
-
-
-
-
-
-    public static boolean palindrom(int[] arr){
+    public static boolean palindrom(int[] arr) {
 
         int left = 0;
         int right = arr.length - 1;
@@ -51,8 +50,7 @@ public class Main {
         return true;  //если все элементы равны с двух сторон, то это палиндром
     }
 
-    public static int countPositive(int[] arr)
-    {
+    public static int countPositive(int[] arr) {
         int countPositive = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > 0) {
@@ -62,9 +60,8 @@ public class Main {
         return countPositive;
     }
 
-    public static int maxAbs(int[] arr)
-    {
-        for (int i= 0; i < arr.length; i++) {
+    public static int maxAbs(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = Math.abs(arr[i]);
         }
         int max = arr[0];
@@ -77,12 +74,12 @@ public class Main {
     }
 
     public static int findLast(int[] arr, int x) {
-        for (int i = arr.length-1; i > 0; i--) {
+        for (int i = arr.length - 1; i > 0; i--) {
             if (x == arr[i]) {
                 return i;
             }
         }
-       //System.out.println("-1"); //элемент не найден
+        //System.out.println("-1"); //элемент не найден
         return -1;
     }
 }
