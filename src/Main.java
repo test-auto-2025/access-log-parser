891df8b8402a27e5f993c6b866f403844305016d
Oleg Maksimov <omaksimov@inno.tech>
2026-02-17 20:15:02 +0300

    Практика Java. Массивы #8

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, -3, 4, -5, -6, 7, 5, 5, -8};
-        int[] arr3 = reverseBack(arr1);
-        System.out.println("Массив 1");
-        for (int i = 0; i <= arr3.length - 1; i++) {
-            System.out.print(arr3[i] + ", "); //выводим массив на экран через запятую
-        }
+        int[] arr1 = {1, 2, -3, 4, -5};
+        int[] arr2 = {-9, 1, 5, 5, -7};
+        int[] arr3 = concat(arr1, arr2);
+        System.out.println("Массив - сложение двух");
+        System.out.print(Arrays.toString(arr3));
+
     }
+
+    public static int[] concat(int[] arr1, int[] arr2) {
+        int count1 = arr1.length;
+        int[] result = new int[count1 + arr2.length];
+        for (int i = 0; i < count1; i++) {
+            result[i] = arr1[i];  //заполняем новый массив элементами первого массива
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            result[count1 + i] = arr2[i];  //заполняем новый массив элементами второго массива
+        }
+        return result;
+    }
+
+
+
+
 
     public static int[] reverseBack(int[] arr) {
         int[] result = new int[arr.length];
@@ -15,13 +33,6 @@ public class Main {
         }
         return result;  //метод меняет порядок элементов массива на обратный
     }
-
-
-
-
-
-
-
 
     public static void reverse(int[] arr) {
 
