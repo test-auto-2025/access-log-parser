d24d4be7da062c8320f013d8629d0f8326f4d336
Oleg Maksimov <omaksimov@inno.tech>
2026-03-21 14:06:09 +0300

    Урок 6 -2 готово

--- src/Line.java
+++ src/Line.java
@@ -12,18 +12,18 @@ public class Line {
     Point p2 = new Point(x2, y2);//constructor chaining
 
     public Line(Point p1, Point p2) {
-        this.x1 = p1.getX();
-        this.y1 = p1.getY();
-        this.x2 = p2.getX();
-        this.y2 = p2.getY();
+        this.x1 = p1.x;
+        this.y1 = p1.y;
+        this.x2 = p2.x;
+        this.y2 = p2.y;
     }
 
-    public double length() {
+    public double getLength() {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     public String toString() {
-        return "Line from (" + x1 + ", " + y1 + ") to (" + x2 + ", " + y2 + ")";
+        return "Линия от (" + x1 + ", " + y1 + ") до (" + x2 + ", " + y2 + ")";
     }
 }
 
--- src/Start.java
+++ src/Start.java
@@ -1,16 +1,38 @@
 public class Start {
 
     public static void main(String[] args) {
+        int a1, b1, //координаты первой точки
+                a2, b2;        //координаты второй точки
+        a1 = 10;
+        b1 = 11;
+        a2 = 15;
+        b2 = 19;
         Point p1 = new Point(1, 3);
-        Point p2 = new Point(1, 3);
-        Point p3 = new Point(5, 8);
+        Point p2 = new Point(5, 8);
+        Point p3 = new Point(a1, b1);
+        Point p4 = new Point(a2, b2);
         System.out.println("Point1 = " + p1);
         System.out.println("Point2 = " + p2);
         System.out.println("Point3 = " + p3);
+        System.out.println("Point4 = " + p4);
 
-        System.out.println("Point1 = Point2: " + (p1 == p2));
-        System.out.println("Point2 = Point3: " + (p2 == p3));
-        System.out.println("Point1 = Point3: " + (p1 == p3));
+        Line l1 = new Line(p1, p2);
+        Line l2 = new Line(a1, b1, a2, b2);
+        Line l3 = new Line(p2, p3);
+        System.out.println("Line1 = " + l1 + " длина: " + l1.getLength());
+        System.out.println("Line2 = " + l2 + " длина: " + l2.getLength());
+        System.out.println("Line3 = " + l3 + " длина: " + l3.getLength());
 
+        System.out.println("Меняем координаты точек линий: ");
+        l1.x2 = l3.x1 = 10;
+        l1.y2 = l3.y1 = 10;
+        l2.x1 = l3.x2 = 7;
+        l2.y1 = l3.y2 = 6;
+        System.out.println("Line1 = " + l1 + " длина: " + l1.getLength());
+        System.out.println("Line2 = " + l2 + " длина: " + l2.getLength());
+        System.out.println("Line3 = " + l3 + " длина: " + l3.getLength());
+
+            double sum = l1.getLength() + l1.getLength() + l2.getLength();
+        System.out.println("Сумма длин =  " + sum);
     }
 }
